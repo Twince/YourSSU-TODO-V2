0b166a1cdae1e53e8cfc79d4a0a073f6dd654cf2
Twince <tjsdn811@gmail.com>
2022-03-28 00:57:27 +0900

    feat: delete start template

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,151 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container">
+        <p class="main_title">Vanilla Js DRAG & DROP TODO</p>
+        <div class="todo_list_box">
+            <div class="none box-detail">
+                <div v-for="(item, index) in noneArr" v-bind:key="item">
+                    <div class="item-container">
+                        <div
+                            ref="itemBlock"
+                            @mousedown="mouseDown"
+                            class="item"
+                        >
+                            {{ index }}, {{ noneArr[index] }}
+                        </div>
+                    </div>
+                </div>
+
+                <div class="add-item">
+                    <input class="value-input" v-model="rawInput" />
+                    <button @click="addTodo" class="commit">test</button>
+                </div>
+            </div>
+            <div ref="itemBlock" class="ready box-detail"></div>
+            <div class="ongoing box-detail"></div>
+            <div class="done box-detail"></div>
+        </div>
+    </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+<script setup>
+import { ref, watchEffect } from "vue";
+
+const noneArr = ref([]);
+const rawInput = ref();
+const flag = ref(false);
+const selectElement = ref();
+
+const mousePosition = ref({
+    left: 0,
+    top: 0,
+});
+
+const mouseDown = (e) => {
+    flag.value = true;
+    selectElement.value = e.target;
+    e.target.parentElement.style.height = "0px";
+
+    mousePosition.value.left = e.offsetX;
+    mousePosition.value.top = e.offsetY;
+};
+
+window.addEventListener("mousemove", (e) => {
+    if (!flag.value) return;
+    e.preventDefault();
+
+    // document.querySelector()
+    selectElement.value.style.position = "absolute";
+    selectElement.value.style.left = `${
+        e.clientX - mousePosition.value.left
+    }px`;
+    selectElement.value.style.top = `${e.clientY - mousePosition.value.top}px`;
+});
+
+window.addEventListener("mouseup", (e) => {
+    if (!flag.value) return;
+    flag.value = false;
+    console.log(e.target, e.offsetY, e.target.classList[0]);
+    selectElement.value.parentElement.style.height = "60px";
+
+    selectElement.value.style.position = "";
+});
+
+watchEffect(() => {
+    console.log(flag.value);
+});
+
+const addTodo = () => {
+    noneArr.value.push(rawInput.value);
+    console.log(noneArr.value);
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
+<style scope>
+.container {
+    width: 100vw;
+    height: 100vh;
+
+    display: block;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.main_title {
+    margin: 0 auto;
+
+    font-size: 30px;
+    font-weight: 700;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.todo_list_box {
+    display: block;
+
+    width: 1200px;
+    height: 500px;
+
+    display: flex;
+    flex-direction: row;
+
+    justify-content: space-around;
+    align-items: center;
+
+    background-color: rgb(163, 163, 163);
 }
-a {
-  color: #42b983;
+
+.box-detail {
+    display: block;
+
+    width: 250px;
+    height: 400px;
+
+    display: flex;
+    flex-direction: column;
+
+    align-items: center;
+
+    background-color: rgb(197, 134, 134);
+}
+
+.item-container {
+    transition: height 0.5s;
+
+    width: 200px;
+    height: 60px;
+}
+
+.item {
+    display: block;
+
+    width: 200px;
+    height: 60px;
+
+    margin-top: 10px;
+
+    border-radius: 7px;
+    background-color: rgb(158, 117, 117);
+
+    cursor: move;
+}
+
+.vavlue-input {
+    transition: 3s;
 }
 </style>
