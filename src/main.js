1c59a8d94d71dff22cfedd5e519cad2478d9e219
Twince <tjsdn811@gmail.com>
2022-04-10 07:30:39 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+// import VueAxios from 'vue-axios'
+
+// const app = Vue.createApp()
+const app = createApp(App) 
+app.config.globalProperties.$axios = axios; 
+app.mount('#app')
+
+// const app = createApp({});
+// createApp(App).mount("#app");
+
